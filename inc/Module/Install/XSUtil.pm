3d1635c91f86865a40cd401e8dff2a27b48946d8
藤 吾郎 <gfuji@cpan.org>
2010-05-14 04:20:42 +0000

    Import of GFUJI/Text-Xslate-0.1014 from CPAN.
    gitpan-cpan-distribution: Text-Xslate
    gitpan-cpan-version:      0.1014
    gitpan-cpan-path:         GFUJI/Text-Xslate-0.1014.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     released

--- inc/Module/Install/XSUtil.pm
+++ inc/Module/Install/XSUtil.pm
@@ -3,7 +3,7 @@ package Module::Install::XSUtil;
 
 use 5.005_03;
 
-$VERSION = '0.23_01';
+$VERSION = '0.23_02';
 
 use Module::Install::Base;
 @ISA     = qw(Module::Install::Base);
@@ -180,7 +180,6 @@ sub cc_append_to_inc{
     for my $dir(@dirs){
         unless(-d $dir){
             warn("'$dir' not found: $!\n");
-            exit;
         }
 
         _verbose "inc: -I$dir" if _VERBOSE;
@@ -198,19 +197,10 @@ sub cc_append_to_inc{
     return;
 }
 
-
 sub cc_libs {
-    goto &cc_append_to_libs;
-}
+    my ($self, @libs) = @_;
 
-sub cc_append_to_libs{
-    my($self, @libs) = @_;
-
-    $self->_xs_initialize();
-
-    my $mm = $self->makemaker_args;
-
-    my $libs = join q{ }, map{
+    @libs = map{
         my($name, $dir) = ref($_) eq 'ARRAY' ? @{$_} : ($_, undef);
         my $lib;
         if(defined $dir) {
@@ -219,18 +209,58 @@ sub cc_append_to_libs{
         else {
             $lib = '';
         }
-        $lib .= ($name =~ /^-/ ? qq{$name } : qq{-l$name});
+        $lib .= ($name =~ /^-/ ? qq{$name} : qq{-l$name});
         _verbose "libs: $lib" if _VERBOSE;
         $lib;
     } @libs;
 
-    if($mm->{LIBS}){
+    $self->cc_append_to_libs( @libs );
+}
+
+sub cc_append_to_libs{
+    my($self, @libs) = @_;
+
+    $self->_xs_initialize();
+
+    return unless @libs;
+
+    my $libs = join q{ }, @libs;
+
+    my $mm = $self->makemaker_args;
+
+    if ($mm->{LIBS}){
         $mm->{LIBS} .= q{ } . $libs;
     }
     else{
         $mm->{LIBS} = $libs;
     }
     return $libs;
+}
+
+sub cc_assert_lib {
+    my ($self, @dcl_args) = @_;
+
+    if ( ! $self->{xsu_loaded_checklib} ) {
+        my $loaded_lib = 0;
+        foreach my $checklib qw(inc::Devel::CheckLib Devel::CheckLib) {
+            eval "use $checklib 0.4";
+            if (!$@) {
+                $loaded_lib = 1;
+                last;
+            }
+        }
+
+        if (! $loaded_lib) {
+            warn "Devel::CheckLib not found in inc/ nor \@INC";
+            exit 0;
+        }
+
+        $self->{xsu_loaded_checklib}++;
+        $self->configure_requires( "Devel::CheckLib" => "0.4" );
+        $self->build_requires( "Devel::CheckLib" => "0.4" );
+    }
+
+    Devel::CheckLib::check_lib_or_exit(@dcl_args);
 }
 
 sub cc_append_to_ccflags{
@@ -552,4 +582,4 @@ sub const_cccmd {
 1;
 __END__
 
-#line 738
+#line 774
