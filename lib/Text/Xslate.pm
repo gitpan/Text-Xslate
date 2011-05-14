5b5327f6fa7832edba9ad0022e569bd712044d1f
藤 吾郎 <gfuji@cpan.org>
2011-05-14 05:38:02 +0000

    Import of GFUJI/Text-Xslate-1.2000 from CPAN.
    gitpan-cpan-distribution: Text-Xslate
    gitpan-cpan-version:      1.2000
    gitpan-cpan-path:         GFUJI/Text-Xslate-1.2000.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     released

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '1.1005_01';
+our $VERSION = '1.2000';
 
 use Carp              ();
 use Fcntl             ();
@@ -570,7 +570,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 1.1005_01.
+This document describes Text::Xslate version 1.2000.
 
 =head1 SYNOPSIS
 
@@ -726,8 +726,7 @@ template names.
 Specifies a function map which contains name-coderef pairs.
 A function C<f> may be called as C<f($arg)> or C<$arg | f> in templates.
 
-There are built-in functions which are not overridable.
-See L<Text::Xslate::Manual::Builtin> for details.
+There are built-in functions described in L<Text::Xslate::Manual::Builtin>.
 
 =item C<< module => [$module => ?\@import_args, ...] >>
 
