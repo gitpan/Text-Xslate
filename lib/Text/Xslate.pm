a8a7960ce9841887afe8b32a1b4af16cfbadf4a4
藤 吾郎 <gfuji@cpan.org>
2013-11-15 13:01:04 +0000

    Import of GFUJI/Text-Xslate-3.0.2 from CPAN.
    gitpan-cpan-distribution: Text-Xslate
    gitpan-cpan-version:      3.0.2
    gitpan-cpan-path:         GFUJI/Text-Xslate-3.0.2.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     released

--- lib/Text/Xslate.pm
+++ lib/Text/Xslate.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '3.0.1';
+our $VERSION = '3.0.2';
 
 use Carp              ();
 use File::Spec        ();
@@ -524,7 +524,7 @@ sub _load_compiled {
         }
         elsif($c->[0] eq 'literal') {
             # force upgrade to avoid UTF-8 key issues
-            utf8::upgrade($c->[1]);
+            utf8::upgrade($c->[1]) if($is_utf8);
         }
         push @asm, $c;
     }
@@ -661,7 +661,7 @@ Text::Xslate - Scalable template engine for Perl5
 
 =head1 VERSION
 
-This document describes Text::Xslate version 3.0.1.
+This document describes Text::Xslate version 3.0.2.
 
 =head1 SYNOPSIS
 
